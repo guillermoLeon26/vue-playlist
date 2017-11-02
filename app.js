8fd59e53c2c444086c7541dfcbb7c2be3fb59603
guillermoLeon26 <gleon2611@gmail.com>
2017-11-02 11:56:56 -0500

    practica5

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ new Vue({
 	data: {
 		name: 'Guillermo',
 		job: 'Ninja',
+		age: 25,
 		website: 'http://www.thenetninja.co.uk',
-		websiteTag: '<a href="http://www.thenetninja.co.uk">website</a>'
+		websiteTag: '<a href="http://www.thenetninja.co.uk">website</a>',
+		x: 0,
+		y: 0
 	},
 	methods:{
 		greet: function (time) {
 			return 'Good '+ time + ' ' + this.name;
+		},
+		add:function (inc) {
+			this.age += inc;
+		},
+		subtract:function (dec) {
+			this.age -= dec;
+		},
+		updateXY:function (event) {
+			this.x = event.offsetX;
+			this.y = event.offsetY;
 		}
 	}
 });
