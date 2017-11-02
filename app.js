5dfc674b8670268c87a2ecc4439035e4e23231fb
guillermoLeon26 <gleon2611@gmail.com>
2017-11-02 16:55:34 -0500

    practica14

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-new Vue({
-	el: '#vue-app',
+var one = new Vue({
+	el: '#vue-app-one',
 	data: {
-		name: 'Guillermo',
-		job: 'Ninja',
-		age: 25,
-		website: 'http://www.thenetninja.co.uk',
-		websiteTag: '<a href="http://www.thenetninja.co.uk">website</a>',
-		x: 0,
-		y: 0
+		title: 'Vue App One'
 	},
 	methods:{
-		greet: function (time) {
-			return 'Good '+ time + ' ' + this.name;
-		},
-		add:function (inc) {
-			this.age += inc;
-		},
-		subtract:function (dec) {
-			this.age -= dec;
-		},
-		updateXY:function (event) {
-			this.x = event.offsetX;
-			this.y = event.offsetY;
+	
+	},
+	computed:{
+		greet:function () {
+			return 'Hello from app one :)'
 		}
 	}
 });
+
+var two = new Vue({
+	el: '#vue-app-two',
+	data: {
+		title: 'Vue App Two'
+	},
+	methods:{
+		changeTitle:function () {
+			one.title = 'Title change'
+		}
+	},
+	computed:{
+		greet:function () {
+			return 'Hello from app two :)'
+		}
+	}
+});
+
+two.title = 'Changed from outside';
