2e1ef3cfe1f0baee9dc5b3b86817afa3bbefdb99
guillermoLeon26 <gleon2611@gmail.com>
2017-11-02 15:59:52 -0500

    practica12

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-		name: 'Guillermo',
-		job: 'Ninja',
-		age: 25,
-		website: 'http://www.thenetninja.co.uk',
-		websiteTag: '<a href="http://www.thenetninja.co.uk">website</a>',
-		x: 0,
-		y: 0
+		name: 'jack',
+		characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+		ninjas: [
+			{name: 'Ryu', age: 25},
+			{name: 'Yoshi', age: 35},
+			{name: 'Ken', age: 55}
+		]
 	},
 	methods:{
-		greet: function (time) {
-			return 'Good '+ time + ' ' + this.name;
-		},
-		add:function (inc) {
-			this.age += inc;
-		},
-		subtract:function (dec) {
-			this.age -= dec;
-		},
-		updateXY:function (event) {
-			this.x = event.offsetX;
-			this.y = event.offsetY;
-		}
+		
+	},
+	computed:{
+		
 	}
 });
