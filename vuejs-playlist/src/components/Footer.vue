3f5a39f5e88d97cc72e0c1c1b7e1e20fbdfa1fca
guillermoLeon26 <gleon2611@gmail.com>
2017-11-03 00:23:44 -0500

    practica21

---
+++ vuejs-playlist/src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <p>{{ copyrith }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      copyrith: 'Copyrith 2017 Vue Ninjas'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  footer{
+    background: #222;
+    padding: 6px;
+  }
+  p{
+    color: lightgreen;
+    text-align: center;
+  }
+</style>
