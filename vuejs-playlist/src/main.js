a2ac61d24a99a611e78c194c173178c42e606b0c
guillermoLeon26 <gleon2611@gmail.com>
2017-11-16 00:08:05 -0500

    practica39

--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResourse from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.use(VueResourse);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+	routes: Routes
+});
 
 //Filters
 Vue.filter('snippet', function (value) {
@@ -26,5 +33,6 @@ Vue.directive('theme', {
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router:router
 })
